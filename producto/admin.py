87450fa63571c5cd010961264084c12e933787bd
Guillermo <guillermotunes@hotmail.com>
2023-05-31 14:27:53 -0300

    reparacion de codigos

--- producto/admin.py
+++ producto/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
-from .models import producto
-admin.site.register(producto)
+from . import models 
 
 
+admin.site.site_title = "Productos"
 
+#@admin.register(models.ProductoCategoria)
+class ProductoCategoriaAdmin(admin.ModelAdmin):
+    """
+    - list_display: tupla que especifica los campos que se mostrarán en la lista de objetos
+    - search_fields: tupla que especifica los campos por los que se puede buscar en la lista de objetos
+    - list_filter: tupla que especifica los campos por los que se puede filtrar en la lista de objetos
+    - ordering: tupla que especifica el orden en que se mostrarán los objetos
+    """
+
+    list_display = ("nombre", "descripcion")
+    search_fields = ("nombre",)
+    list_filter = ("nombre",)
+    ordering = ("nombre",)
--- producto/models.py
+++ producto/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class producto(models.Model):
-    producto = models.CharField(max_length=50)
-    cantidad = models.CharField(max_length=50)
-    precio = models.CharField(max_length=50)
 
+class ProductoCategoria(models.Model):
+
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.CharField(max_length=250, null=True, blank=True, verbose_name="descripción")
 
     def __str__(self):
-        return self.producto
-    
+        return self.nombre
